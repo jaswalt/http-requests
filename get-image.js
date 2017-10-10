9a6e93d4d00c3976ccac52061cfd3132f5ef0ffa
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-10 04:01:39 +0000

    initial commit

---
+++ get-image.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://syntantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
+        throw err;
+       })
+       .on('response', function (response) {
+        console.log('Response Status Code: ', response.statusCode,'\n',
+                    'Response Status Message: ', response.statusMessage,'\n',
+                    'Content Type: ', response.headers['content-type']);
+        console.log('Download complete.')
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
+          console.log('Downloading image...');
